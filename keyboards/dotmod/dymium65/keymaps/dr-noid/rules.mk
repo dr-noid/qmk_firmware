929f23433a35ca441297bd64644a57553e82804b
dr-noid <beratkir61@gmail.com>
2024-11-01 14:12:59 +0100

    fix encoder not working

--- keyboards/dotmod/dymium65/keymaps/dr-noid/rules.mk
+++ keyboards/dotmod/dymium65/keymaps/dr-noid/rules.mk
@@ -4,11 +4,12 @@ VIA_ENABLE = yes
 MAGIC_ENABLE = yes
 LTO_ENABLE = yes
 
+# One of these makes the encoder stop working but cba figuring out which one
 # Disabled
-CONSOLE_ENABLE = no
-COMMAND_ENABLE = no
-MOUSEKEY_ENABLE = no
-EXTRAKEY_ENABLE = no
-MUSIC_ENABLE = no
-GRAVE_ESC_ENABLE = no
-SPACE_CADET_ENABLE = no
+# CONSOLE_ENABLE = no
+# COMMAND_ENABLE = no
+# MOUSEKEY_ENABLE = no
+# EXTRAKEY_ENABLE = no
+# MUSIC_ENABLE = no
+# GRAVE_ESC_ENABLE = no
+# SPACE_CADET_ENABLE = no
