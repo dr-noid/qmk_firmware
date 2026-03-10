7eb0ba7e7ca793e7b738fa1865a7994d0a23df87
dr-noid <beratkir61@gmail.com>
2026-03-10 03:07:51 +0100

    update keymap

--- keyboards/dotmod/dymium65/keymaps/dr-noid/keymap.c
+++ keyboards/dotmod/dymium65/keymaps/dr-noid/keymap.c
@@ -19,43 +19,62 @@
 
 // Layer names
 enum ats_layers {
-    // - Base layer:
-    _BASE,
-    // - Symbols, numbers, and functions:
-    _FN,
-    // - Alternate Function layer:
-    _LN,
-    // - Alternate Function layer:
-    _MT,
+    // Base layers
+    QWERTY_LAYER,
+    CANARY_LAYER,
+    // F1 through F12
+    FUNC_LAYER,
+    // Alternate Function layer
+    ALT_LAYER
 };
+
+// Custom keycodes
+enum custom_keycodes {
+    TG_LAYOUT = SAFE_RANGE,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case TG_LAYOUT:
+            if (record->event.pressed) {
+                if (get_highest_layer(default_layer_state) == CANARY_LAYER) {
+                    default_layer_set(1UL << QWERTY_LAYER);
+                } else {
+                    default_layer_set(1UL << CANARY_LAYER);
+                }
+            }
+            return false;
+    }
+    return true;
+}
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BASE] = LAYOUT(
-    KC_ESC,        KC_1,        KC_2,        KC_3,  KC_4,  KC_5,  KC_6,      KC_7,  KC_8,  KC_9,         KC_0,         KC_MINUS,        KC_EQUAL,         KC_CAPS,   KC_AUDIO_MUTE,
-    KC_TAB,        KC_Q,        KC_W,        KC_E,  KC_R,  KC_T,  KC_Y,      KC_U,  KC_I,  KC_O,         KC_P,         KC_LEFT_BRACKET, KC_RIGHT_BRACKET, KC_BACKSLASH,   KC_PSCR,
-    KC_BSPC,       KC_A,        KC_S,        KC_D,  KC_F,  KC_G,  KC_H,      KC_J,  KC_K,  KC_L,         KC_SEMICOLON, KC_QUOTE,                          KC_ENTER,       KC_DEL,
+    [QWERTY_LAYER] = LAYOUT(
+    KC_ESC,        KC_1,        KC_2,        KC_3,  KC_4,  KC_5,  KC_6,      KC_7,  KC_8,  KC_9,         KC_0,         KC_MINUS,        KC_EQUAL,         KC_CAPS_LOCK,   KC_AUDIO_MUTE,
+    KC_TAB,        KC_Q,        KC_W,        KC_E,  KC_R,  KC_T,  KC_Y,      KC_U,  KC_I,  KC_O,         KC_P,         KC_LEFT_BRACKET, KC_RIGHT_BRACKET, KC_BACKSLASH,   KC_PRINT_SCREEN,
+    KC_BACKSPACE,  KC_A,        KC_S,        KC_D,  KC_F,  KC_G,  KC_H,      KC_J,  KC_K,  KC_L,         KC_SEMICOLON, KC_QUOTE,                          KC_ENTER,       KC_DELETE,
     KC_LEFT_SHIFT,              KC_Z,        KC_X,  KC_C,  KC_V,  KC_B,      KC_N,  KC_M,  KC_COMMA,     KC_DOT,       KC_SLASH,                          KC_RIGHT_SHIFT, KC_UP,
-    KC_LEFT_CTRL,  KC_LEFT_GUI, KC_LEFT_ALT,                      KC_SPACE,                KC_RIGHT_ALT, MO(_FN),                       KC_RIGHT_CTRL,    KC_LEFT,        KC_DOWN,        KC_RIGHT
+    KC_LEFT_CTRL,  KC_LEFT_GUI, KC_LEFT_ALT,                      KC_SPACE,                KC_RIGHT_ALT, MO(FUNC_LAYER),                KC_RIGHT_CTRL,    KC_LEFT,        KC_DOWN,        KC_RIGHT
     ),
-    [_FN] = LAYOUT(
-    KC_GRAVE,      KC_F1,       KC_F2,       KC_F3, KC_F4, KC_F5, KC_F6,     KC_F7, KC_F8, KC_F9,        KC_F10,       KC_F11,          KC_F12,           KC_CAPS,   KC_TRNS,
-    RGB_TOG,       KC_Q,        KC_W,        KC_E,  KC_R,  KC_T,  KC_Y,      KC_U,  KC_I,  KC_O,         KC_P,         RGB_HUI,         RGB_HUD,          KC_BACKSLASH,   KC_F13,
-    KC_BSPC,       KC_A,        KC_S,        KC_D,  KC_F,  KC_G,  KC_H,      KC_J,  KC_K,  KC_L,         RGB_SAD,      RGB_SAI,                           KC_ENTER,       KC_F14,
-    KC_LEFT_SHIFT,              KC_Z,        KC_X,  KC_C,  KC_V,  KC_B,      KC_N,  KC_M,  KC_COMMA,     _______,      KC_SLASH,                          KC_RIGHT_SHIFT, RGB_VAI,
-    KC_LEFT_CTRL,  GU_TOGG, KC_LEFT_ALT,                      KC_SPACE,                RGB_MOD,      _______,                       MO(_LN),    RGB_SPD,        RGB_VAD,        RGB_SPI
+    [CANARY_LAYER] = LAYOUT(
+    KC_ESC,        KC_1,        KC_2,        KC_3,  KC_4,  KC_5,  KC_6,      KC_7,  KC_8,  KC_9,         KC_0,         KC_MINUS,        KC_EQUAL,         KC_CAPS_LOCK,   KC_AUDIO_MUTE,
+    KC_TAB,        KC_W,        KC_L,        KC_Y,  KC_P,  KC_K,  KC_Z,      KC_X,  KC_O,  KC_U,         KC_SEMICOLON, KC_LEFT_BRACKET, KC_RIGHT_BRACKET, KC_BACKSLASH,   KC_PRINT_SCREEN,
+    KC_BACKSPACE,  KC_C,        KC_R,        KC_S,  KC_T,  KC_B,  KC_F,      KC_N,  KC_E,  KC_I,         KC_A,         KC_QUOTE,                          KC_ENTER,       KC_DELETE,
+    KC_LEFT_SHIFT,              KC_J,        KC_V,  KC_D,  KC_G,  KC_Q,      KC_M,  KC_H,  KC_SLASH,     KC_COMMA,     KC_DOT,                            KC_RIGHT_SHIFT, KC_UP,
+    KC_LEFT_CTRL,  KC_LEFT_GUI, KC_LEFT_ALT,                      KC_SPACE,                KC_RIGHT_ALT, MO(FUNC_LAYER),                KC_RIGHT_CTRL,    KC_LEFT,        KC_DOWN,        KC_RIGHT
     ),
-    [_LN] = LAYOUT(
-    KC_ESC,        KC_1,        KC_2,        KC_3,  KC_4,  KC_5,  KC_6,      KC_7,  KC_8,  KC_9,         KC_0,         KC_MINUS,        KC_EQUAL,         KC_CAPS,   KC_TRNS,
+    [FUNC_LAYER] = LAYOUT(
+    KC_GRAVE,      KC_F1,       KC_F2,       KC_F3, KC_F4, KC_F5, KC_F6,     KC_F7, KC_F8, KC_F9,        KC_F10,       KC_F11,          KC_F12,           TG_LAYOUT,      KC_TRNS,
+    RM_TOGG,       KC_Q,        KC_W,        KC_E,  KC_R,  KC_T,  KC_Y,      KC_U,  KC_I,  KC_O,         KC_P,         RM_HUEU,         RM_HUED,          KC_BACKSLASH,   KC_F13,
+    KC_BACKSPACE,  KC_A,        KC_S,        KC_D,  KC_F,  KC_G,  KC_H,      KC_J,  KC_K,  KC_L,         RM_SATD,      RM_SATU,                           KC_ENTER,       KC_F14,
+    KC_LEFT_SHIFT,              KC_Z,        KC_X,  KC_C,  KC_V,  KC_B,      KC_N,  KC_M,  KC_COMMA,     KC_DOT,       KC_SLASH,                          KC_RIGHT_SHIFT, RM_VALU,
+    KC_LEFT_CTRL,  GU_TOGG,     KC_LEFT_ALT,                      KC_SPACE,                RM_NEXT,      _______,                       MO(ALT_LAYER),    RM_SPDD,        RM_VALD,        RM_SPDU
+    ),
+    [ALT_LAYER] = LAYOUT(
+    KC_ESC,        KC_1,        KC_2,        KC_3,  KC_4,  KC_5,  KC_6,      KC_7,  KC_8,  KC_9,         KC_0,         KC_MINUS,        KC_EQUAL,         KC_CAPS_LOCK,   KC_TRNS,
     KC_TAB,        KC_Q,        KC_W,        KC_E,  KC_R,  KC_T,  KC_Y,      KC_U,  KC_I,  KC_O,         KC_P,         KC_LEFT_BRACKET, KC_RIGHT_BRACKET, KC_BACKSLASH,   KC_TRNS,
-    KC_BSPC,  KC_A,        KC_S,        KC_D,  KC_F,  KC_G,  KC_H,      KC_J,  KC_K,  KC_L,         KC_SEMICOLON, KC_QUOTE,                          KC_ENTER,       KC_TRNS,
-    KC_LEFT_SHIFT,              KC_Z,        KC_X,  KC_C,  KC_V,  KC_B,      KC_N,  KC_M,  _______,      _______,      KC_SLASH,                          KC_RIGHT_SHIFT, KC_UP,
-    KC_LEFT_CTRL,  KC_LEFT_GUI, KC_LEFT_ALT,                      KC_SPACE,                KC_RIGHT_ALT, _______,                       _______,    KC_LEFT,        KC_DOWN,        KC_RIGHT
-    ),
-    [_MT] = LAYOUT(
-    KC_ESC,        KC_1,        KC_2,        KC_3,  KC_4,  KC_5,  KC_6,      KC_7,  KC_8,  KC_9,         KC_0,         KC_MINUS,        KC_EQUAL,         KC_CAPS,   KC_TRNS,
-    KC_TAB,        KC_Q,        KC_W,        KC_E,  KC_R,  KC_T,  KC_Y,      KC_U,  KC_I,  KC_O,         KC_P,         KC_LEFT_BRACKET, KC_RIGHT_BRACKET, KC_BACKSLASH,   KC_TRNS,
-    KC_BSPC,  KC_A,        KC_S,        KC_D,  KC_F,  KC_G,  KC_H,      KC_J,  KC_K,  KC_L,         KC_SEMICOLON, KC_QUOTE,                          KC_ENTER,       KC_TRNS,
+    KC_BACKSPACE,  KC_A,        KC_S,        KC_D,  KC_F,  KC_G,  KC_H,      KC_J,  KC_K,  KC_L,         KC_SEMICOLON, KC_QUOTE,                          KC_ENTER,       KC_TRNS,
     KC_LEFT_SHIFT,              KC_Z,        KC_X,  KC_C,  KC_V,  KC_B,      KC_N,  KC_M,  _______,      _______,      KC_SLASH,                          KC_RIGHT_SHIFT, KC_UP,
     KC_LEFT_CTRL,  KC_LEFT_GUI, KC_LEFT_ALT,                      KC_SPACE,                KC_RIGHT_ALT, _______,                       KC_RIGHT_CTRL,    KC_LEFT,        KC_DOWN,        KC_RIGHT
     )
@@ -64,9 +83,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [_BASE] = {ENCODER_CCW_CW(KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP)},
-    [_FN]   = {ENCODER_CCW_CW(KC_F15, KC_F16)},
-    [_LN]   = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
-    [_MT]   = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
+    [QWERTY_LAYER] = {ENCODER_CCW_CW(KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP)},
+    [CANARY_LAYER] = {ENCODER_CCW_CW(KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP)},
+    [FUNC_LAYER]   = {ENCODER_CCW_CW(KC_F15, KC_F16)},
+    [ALT_LAYER]    = {ENCODER_CCW_CW(RM_SATD, RM_SATU)},
 };
 #endif
--- keyboards/dotmod/dymium65/keymaps/dr-noid/readme.md
+++ keyboards/dotmod/dymium65/keymaps/dr-noid/readme.md
@@ -1,11 +1,22 @@
 # keycodes
 
--   F13 (fn + prtsc) -> mute discord
--   F14 (fn + del) -> deafen discord
+- F13 (fn + prtsc) -> mute discord
+- F14 (fn + del) -> deafen discord
 
--   F15 (fn + encoder) -> decrease brightness
--   F16 (fn + encoder) -> increase brightness
+- F15 (fn + encoder) -> decrease brightness
+- F16 (fn + encoder) -> increase brightness
 
--   `fn` + `GUI` -> toggle GUI
+- `fn` + `GUI` -> toggle GUI
+- `fn` + `Caps Lock` -> toggle QWERTY / Canary layout
 
 I use [twinkle-tray](https://github.com/xanderfrangos/twinkle-tray) for brightness control
+
+## Canary layout (row stagger)
+
+Reference: [github.com/Apsu/Canary](https://github.com/Apsu/Canary)
+
+```
+w l y p k   z x o u ;
+c r s t b   f n e i a '
+j v d g q   m h / , .
+```
